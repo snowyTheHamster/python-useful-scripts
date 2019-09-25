fcf0ce0c605748ef98bae31a3bfea99fd43b2bf1
ttgo <vidafuku@gmail.com>
2019-09-25 09:44:18 +0800

    added windows 10 screen res switcher for surface go

---
+++ surfacego-res-1800.py
@@ -0,0 +1,43 @@
+# changes the screen resolution to 1800x1200
+# on windows 10
+import pyautogui as pag
+import time
+
+pag.FAILSAFE = True
+print('move cursor to top left of screen to force quit the app.')
+
+# Open Windows "Display Settings"
+pag.press('winleft')
+pag.typewrite('display settings')
+time.sleep(2)
+pag.press('enter')
+time.sleep(5)
+
+# tab down to the display resolution option
+for i in range(10):
+    pag.press('tab')
+time.sleep(2)
+
+# select the screen resolution here
+pag.press('space')
+time.sleep(1)
+pag.press('pageup')
+time.sleep(1)
+pag.press('down')
+time.sleep(1)
+pag.press('space')
+time.sleep(1)
+
+# select "keep changes" if prompted
+pag.press('tab')
+time.sleep(1)
+pag.press('space')
+time.sleep(1)
+
+# close window
+pag.keyDown('altleft')
+pag.keyDown('space')
+pag.keyUp('space')
+pag.keyUp('altleft')
+time.sleep(1)
+pag.typewrite('c')
